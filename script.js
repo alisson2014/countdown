b6e106b1ed83b7048e97d9fbaa15193cda13557a
Alisson Vinícius <almeidaalisson2014@gmail.com>
2023-06-17 19:42:09 -0300

    Fiz alguns ajustes ao projeto

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-const daysEl = document.getElementById("days")
-const hoursEl = document.getElementById("hours")
-const minutesEl = document.getElementById("minutes")
-const secondsEl = document.getElementById("seconds")
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
 
-const launch = "1 Feb 2023"
+const launch = "1 Dec 2023";
 
 const countdown = () => {
-  const launchDate = new Date(launch)
-  const currentDate = new Date()
+  const launchDate = new Date(launch);
+  const currentDate = new Date();
 
-  const totalSeconds = (launchDate - currentDate) / 1000
-  const days = Math.floor(totalSeconds / 3600 / 24)
-  const hours = Math.floor(totalSeconds / 3600) % 24
-  const minutes = Math.floor(totalSeconds / 60) % 60
-  const seconds = Math.floor(totalSeconds) % 60
+  const totalSeconds = (launchDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  daysEl.innerHTML = days
-  hoursEl.innerHTML = formatTime(hours)
-  minutesEl.innerHTML = formatTime(minutes)
-  secondsEl.innerHTML = formatTime(seconds)
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = ": " + formatTime(minutes);
+  secondsEl.innerHTML = ": " + formatTime(seconds);
 }
 
-const formatTime = (time) => (time < 10 ? `0${time}` : time)
+const formatTime = time => time < 10 ? `0${time}` : time;
 
 countdown()
 
